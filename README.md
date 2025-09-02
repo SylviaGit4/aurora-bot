dc37134f103594a9b970137cccf7cfb439866c54
SylviaGit4 <torchicky139@gmail.com>
2025-09-02 01:16:59 +0100

    First commands.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aurora-bot
 Discord bot testbed.
+
+My personal bot to test how programming a discord bot works.
--- bot.py
+++ bot.py
@@ -6,8 +6,30 @@ CHANNEL_ID = 1412224167537016922
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
+#Shows the bot is up and running.
 @bot.event
 async def on_ready():
     print("Bot running.")
+    channel = bot.get_channel(CHANNEL_ID)
+    await channel.send("Bot running.")
+
+#Shows list of commands.
+@bot.command()
+async def commands(ctx):
+    await ctx.send("# Commands List \n!commands - Shows list of commands. \n!test_up - Test to see if bot is correctly recieving commands. \n!sum - Adds numbers and shows result.")
+
+#Checks to see if the bot is correctly recieving commands.
+@bot.command()
+async def test_up(ctx):
+    await ctx.send("Bot currently up.")
+
+#Shows the sum of numbers in a message.
+@bot.command()
+async def sum(ctx, *arr):
+    result = 0
+    for i in arr:
+        result += int(i)
+    await ctx.send(f"Result: {result}")
+
 
 bot.run(BOT_TOKEN)
