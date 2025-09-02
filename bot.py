53ee55563eae28a791cd1564f3868d16de6b8651
SylviaGit4 <torchicky139@gmail.com>
2025-09-02 01:35:09 +0100

    Moved token to external file.

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import discord
 from discord.ext import commands
+import json
 
-BOT_TOKEN = "MTQxMTc4OTMxNDQyOTU1MDY3Ng.GX_YCv.rLzvA_qoj2Lcb5JL1DxQYOEKirrO4Go2pQ7HWc"
+with open('config.json', 'r') as cfg:
+    data = json.load(cfg)
+
+BOT_TOKEN = data["token"]
 CHANNEL_ID = 1412224167537016922
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
@@ -16,7 +20,7 @@ async def on_ready():
 #Shows list of commands.
 @bot.command()
 async def commands(ctx):
-    await ctx.send("# Commands List \n!commands - Shows list of commands. \n!test_up - Test to see if bot is correctly recieving commands. \n!")
+    await ctx.send("# Commands List \n!commands - Shows list of commands. \n!test_up - Test to see if bot is correctly recieving commands. \n")
 
 #Checks to see if the bot is correctly recieving commands.
 @bot.command()
